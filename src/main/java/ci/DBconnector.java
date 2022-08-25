347df7233cff9f7811a59adad4611eb95ad2bf95
Patrick Nielsen <cph-pn157@cphbusiness.dk>
2022-08-25 12:40:12 +0200

    maventest2

--- src/main/java/ci/DBconnector.java
+++ src/main/java/ci/DBconnector.java
@@ -19,6 +19,7 @@ public class DBconnector {
         if (singleton == null) {
             Class.forName("com.mysql.cj.jdbc.Driver");
             singleton = DriverManager.getConnection(URL, USER, PW);
+
         }
         return singleton;
     }
