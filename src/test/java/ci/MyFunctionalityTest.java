987d3913476c19e033e3e6c17fd0a407bc75e1a0
Patrick Nielsen <cph-pn157@cphbusiness.dk>
2022-09-30 00:17:34 +0200

    test fejler

--- src/test/java/ci/MyFunctionalityTest.java
+++ src/test/java/ci/MyFunctionalityTest.java
@@ -47,7 +47,7 @@ public class MyFunctionalityTest {
     public void testGetTen() {
 
         MyFunctionality instance = new MyFunctionality();
-        assertEquals(10, instance.getTen());
+        assertEquals(11, instance.getTen());
     }
 }
 
